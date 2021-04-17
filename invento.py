936e4d2008a747be4a451a2631349f297e8e8927
Guevados <waldogg226@gmail.com>
2021-04-17 19:43:49 -0400

    commint inicial

---
+++ invento.py
@@ -0,0 +1,38 @@
+class Composer:
+    def __init__(self, name, lastname, nationality, period):
+        self.Name = name
+        self.LastName = lastname
+        self.Nationality = nationality
+        self.Period = period
+    def SeeComposer(self):
+        print("Composer: ",self.Name," ",self.LastName," ",self.Nationality," ",self.Period)
+
+class Compositions:
+    def __init__(self, title, instrumentation, tipe):
+        self.Title = title
+        self.Instrumentation = instrumentation
+        self.Tipe = tipe
+    def AddComposer(self, composer):
+        self.Composer = composer
+    def SeeComposition(self):
+        print("Title: ", self.Title)
+        print("Instrumentation ",self.Instrumentation)
+        print("Tipe ",self.Tipe)
+        self.Composer.SeeComposer()
+    def GetTitle(self):
+        return self.Title
+
+class Librery(Composer,Compositions):
+    def __init__(self):
+        self.ListCompositions = []
+    def NumsCompositions(self):
+        return len(self.ListCompositions)
+    def AddComposition(self, title):
+        self.ListCompositions = self.ListCompositions + [Compositions]
+    def SeeLibrery(self):
+        print("###########################")
+        for item in self.ListCompositions:
+            item.SeeComposition()
+        print("###########################")
+    
+        
