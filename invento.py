ad2cb8618e9b00f518c5e0be39f2cf593d02cc39
Guevados <waldogg226@gmail.com>
2021-04-18 13:57:41 -0400

    cambio 5

--- invento.py
+++ invento.py
@@ -47,4 +47,50 @@ class Librery(Composer,Compositions):
             print("Deleted work correctly!")
         else:
             print("Work not found!")
-        
+
+def SeeMenu():
+    print ("""Menu
+1) Add composition to librery
+2) See Librery
+3) Delete composition
+4) Number of compositions
+5) Exit""")
+
+def AddCompositionLibrery(librery):
+    title = input("Enter the title of the composition: ")
+    instrumentation = input("Enter the instrumentation: ")
+    composername = input("Enter the name to composer: ")
+    composerlastname = input("Enter the last name to composer: ")
+    composer = Composer(composername, composerlastname)
+    composition = Compositions(title, instrumentation)
+    composition.AddComposer(composer)
+    librery.AddComposition(composition)
+    return librery
+def SeeLibrery(librery):
+    librery.SeeLibrery()
+
+def DelComposition(librery):
+    title = input("Enter the titleof the composition to delete: ")
+    librery.DelComposition(title)
+
+def NumsComposition(librery):
+    print("The numbers of compositions in the librery is: ",librery.NumsComposition())
+
+end = False
+librery = Librery()
+
+while not end:
+    SeeMenu()
+    opc = int(input("Select option: "))
+    if (opc == 1):
+        librery = AddCompositionLibrery(librery)
+    elif (opc == 2):
+        SeeLibrery(librery)
+    elif(opc == 3):
+        DelComposition(librery)
+    elif(opc == 4):
+        NumsComposition(librery)
+    elif(opc == 5):
+        end = True
+
+print("Good Bye!!!")
